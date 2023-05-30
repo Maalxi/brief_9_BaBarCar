3131caa1166fb79503e870d5550677589cccc9ec
Maalxi <maalxi38@gmail.com>
2023-05-30 13:51:19 +0200

    Mise à jour le 30-05-2023 - update

---
+++ migrations/Version20230530114758.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230530114758 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE car (id INT AUTO_INCREMENT NOT NULL, owner_id INT DEFAULT NULL, brand VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, seats INT NOT NULL, created DATE NOT NULL, INDEX IDX_773DE69D7E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE car ADD CONSTRAINT FK_773DE69D7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car DROP FOREIGN KEY FK_773DE69D7E3C61F9');
+        $this->addSql('DROP TABLE car');
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\AnimalSex;
+use App\Entity\User;
+use DateTime;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends AbstractFixture
+{
+    public const REF_USER = self::class;
+
+
+    public function load(ObjectManager $manager): void
+    {
+        $adminUser = new User();
+        $adminUser->setEmail('admin@gmail.com');
+        $adminUser->setRoles([]);
+        
+        // $adminUser->setVerified(1);
+        
+        $adminUser->setPassword('$2y$13$YWUuFI7lRCxOmMI7xSlMd.6TGuTcMtXzRfp8KhjvhSytNiJA1CJ3a');
+        
+        $adminUser->setFirstName($this->faker->firstName());
+        $adminUser->setLastName($this->faker->lastName());
+
+        $adminUser->setPhone($this->faker->phoneNumber());
+
+        $dateString = $this->faker->date(); // Example date string
+        $createdDate = new \DateTime($dateString); // Convert the string to a DateTime object
+        
+        $adminUser->setCreated($createdDate);
+
+        $manager->persist($adminUser);
+
+        $this->setReference(self::REF_USER . '_' . 0, $adminUser);
+
+        for ($i = 1; $i < 6; $i++) {
+            $user = new User();
+            $user->setEmail($this->faker->email());
+            $user->setRoles([]);
+
+            // $user->setVerified(1);
+
+            $user->setPassword($this->passwordHasher->hashPassword($user, '12345678'));
+
+            $user->setFirstName($this->faker->firstName());
+            $user->setLastName($this->faker->lastName());
+    
+            $user->setPhone($this->faker->phoneNumber());
+    
+            $dateString = $this->faker->date(); // Example date string
+            $createdDate = new \DateTime($dateString); // Convert the string to a DateTime object
+            
+            $user->setCreated($createdDate);
+
+            $manager->persist($user);
+
+            $this->setReference(self::REF_USER . '_' . $i, $user);
+        }
+
+        $manager->flush();
+    }
+}
