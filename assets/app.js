9bcb98f6cbcbb92595db602b75ca847b01c8f054
Maalxi <maalxi38@gmail.com>
2023-05-31 16:58:19 +0200

    Mise à jour le 31-05-2023 - update

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import './styles/navbar.scss';
+import './styles/homepage.scss';
+
+// start the Stimulus application
+import './bootstrap';
