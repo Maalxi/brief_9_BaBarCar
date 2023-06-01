1b95b0a550ad2fa835a346e9b249c6b22bc40760
Maalxi <maalxi38@gmail.com>
2023-06-01 14:10:52 +0200

    Mise à jour le 01-06-2023 - update

---
+++ src/Controller/TrajetController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrajetController extends AbstractController
+{
+    #[Route('/trajet', name: 'app_trajet')]
+    public function index(): Response
+    {
+        return $this->render('trajet/index.html.twig', [
+            'controller_name' => 'TrajetController',
+        ]);
+    }
+    #[Route('/trajet/publication', name: 'app_trajet')]
+    public function publication(): Response
+    {
+        return $this->render('trajet/publication.html.twig', [
+            'controller_name' => 'TrajetController',
+        ]);
+    }
+}
