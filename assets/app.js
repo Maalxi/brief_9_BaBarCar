1cb3cc7d18ef3cbb43f83fb7c2788afd7f487632
Maalxi <maalxi38@gmail.com>
2023-06-08 10:26:08 +0200

    Mise à jour le 08-06-2023 - update

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,15 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
-import './styles/navbar.scss';
-import './styles/homepage.scss';
-import './styles/details.scss';
+import "./styles/app.scss";
+import "./styles/navbar.scss";
+import "./styles/homepage.scss";
+import "./styles/details.scss";
 
-// start the Stimulus application
-import './bootstrap';
+import "@popperjs/core";
+
+import * as bootstrap from "bootstrap";
+
+
+
+
---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+    #[Route('/profil', name: 'app_profil')]
+    public function index()
+    {
+        $user = $this->getUser(); // Récupère l'utilisateur connecté
+
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+            'user' => $user,
+        ]);
+    }
+}
